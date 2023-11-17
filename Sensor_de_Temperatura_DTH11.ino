0e6a7ccd22f158cb2247edc272366e8bcd5371b9
Camilo7710 <148549892+Camilo7710@users.noreply.github.com>
2023-11-17 07:49:47 -0500

    Add files via upload

---
+++ Sensor_de_Temperatura_DTH11.ino
@@ -0,0 +1,22 @@
+#include <Bonezegei_DHT11.h>
+
+//param = DHT11 signal pin
+Bonezegei_DHT11 dht(D4);
+float TEMPCEL;
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  pinMode (D3, OUTPUT);
+  
+}
+
+void loop() {
+
+  if (dht.getData()) {                         
+    TEMPCEL = dht.getTemperature();   
+       int hum = dht.getHumidity();               
+    Serial.printf("Temperatura: %0.1lf°C  Humedad:%d \n", TEMPCEL,  hum);
+  }
+  delay(500);  
+}
